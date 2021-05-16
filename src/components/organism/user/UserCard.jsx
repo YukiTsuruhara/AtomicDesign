36c19eab96c7092ab39c32677dd1b18df285203a
YukiTsuruhara <81816095+YukiTsuruhara@users.noreply.github.com>
2021-05-16 23:29:47 +0900

    Initial commit

---
+++ src/components/organism/user/UserCard.jsx
@@ -0,0 +1,18 @@
+export const UserCard = () => {
+  return (
+    <div>
+      <img src="" alt="プロフィール" />
+      <p>氏名</p>
+      <dl>
+        <dt>メール</dt>
+        <dd>aaaa@gmail.com</dd>
+        <dt>電話番号</dt>
+        <dd>000-999-888</dd>
+        <dt>会社名</dt>
+        <dd>デューク株式会社</dd>
+        <dt>WEB</dt>
+        <dd>https://aaa.com</dd>
+      </dl>
+    </div>
+  );
+};
